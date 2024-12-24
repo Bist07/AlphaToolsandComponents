c5d76b6fd59dced84b4ecb5badcd604662f880db
Bist07 <74443096+Bist07@users.noreply.github.com>
2024-12-24 12:42:59 -0800

    replaced default react logo with a custom logo

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://www.alphatnc.ca',
+    origin: 'https://www.alphatnc.ca',
     methods: ['GET', 'POST'],
     credentials: true
 }));
