0955585c2c51808d3a11dadd818960a5c3a78c00
Bist07 <abhiek7@gmail.com>
2025-04-01 07:56:14 -0700

    refactor: remove unused dark theme palette configuration

--- client/src/theme/AppTheme.js
+++ client/src/theme/AppTheme.js
@@ -20,7 +20,7 @@ function AppTheme(props) {
           colorSchemeSelector: 'data-mui-color-scheme',
           cssVarPrefix: 'template',
         },
-        colorSchemes, // Recently added in v6 for building light & dark mode app, see https://mui.com/material-ui/customization/palette/#color-schemes
+        // Recently added in v6 for building light & dark mode app, see https://mui.com/material-ui/customization/palette/#color-schemes
         typography,
         shadows,
         shape,
